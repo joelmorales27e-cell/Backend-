24e9c83ff5a1d01b12c1f416977dcfe62ef0cda2
joelmorales27e <joelmorales27e@gmail.com>
2025-10-17 08:39:07 -0400

    commit

--- db.js
+++ db.js
@@ -2,13 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DATABASE_URL,
   {
-    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
-    logging: false
   }
 );
 
